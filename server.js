7011f0baf8e8aba7ad3a27487cc366913a9fd92b
Danil Ivanov <danilivanov.ivanov@gmail.com>
2017-11-12 04:26:00 +0100

    removed etags

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.use(bodyParser.urlencoded({ extended:true }));
 app.use(bodyParser.json());
 
 var routes = require('./api/routes/routes');
+app.set('etag', false);
 routes(app);
 
 app.listen(port);
